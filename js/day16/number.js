12f0755b06524cf3b6b79c9c4a67e161ab2547f6
Aryan Dev <aryanatwork45@gmail.com>
2026-06-14 23:03:09 +0530

    Added Extension, Number file and an img

---
+++ js/day16/number.js
@@ -0,0 +1,11 @@
+
+
+btn.addEventListener("click", function() {
+    const n1 = Number(document.getElementById('num1').value)
+
+const n2 = Number(document.getElementById('num2').value)
+    const n3 = n1 + n2
+
+
+console.log(n3)
+})
---
+++ js/day16/templatestring.js
@@ -0,0 +1,21 @@
+const recipient = "James"
+
+const email = "Hey" + recipient + "! How is it going? Cheers Per"
+
+const myname = "Aryan"
+
+const template = `
+Hey ${recipient}! 
+
+How is it going? 
+
+Cheers Per
+`
+
+const mymessage = `Hey ${recipient}! How is it going? Cheers ${myname}`
+
+console.log(mymessage)
+
+console.log(template)
+
+console.log(email)
