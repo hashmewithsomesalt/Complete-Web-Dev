6f3d2d4c8140faefbfa57d434d0009364e0dba16
Aryan Dev <aryanatwork45@gmail.com>
2026-06-17 20:11:38 +0530

    Modified and added icon in extension1

--- js/day16/extension1.js
+++ js/day16/extension1.js
@@ -1,12 +1,41 @@
 const inputBtn = document.getElementById("input-btn")
-let myLeads = []
+let myLeads = ["www.google.com"]
+//turn my leads into string
+/* myLeads = JSON.stringify(myLeads)
+console.log(typeof myLeads)
+console.log(myLeads)
+
+myLeads = JSON.parse(myLeads)
+console.log(typeof myLeads)
+console.log(myLeads)
+
+myLeads.push("www.aryan.com")
+
+myLeads = JSON.stringify(myLeads)
+console.log(typeof myLeads)
+console.log(myLeads) */
+
 const inputEl = document.getElementById("input-el")
 const ulEl = document.getElementById("ul-el")
 
+/* localStorage.setItem("myLeads","www.example")
+localStorage.setItem("myLead","www.examp")
+localStorage.setItem("myLea","www.exame")
+console.log(localStorage.getItem("myLeads"))
+console.log(localStorage.getItem("myLead"))
+console.log(localStorage.getItem("myLea"))
+localStorage.clear()
+localStorage.clear()
+localStorage.clear() */
+
+let leadsFromLocalstorage = JSON.parse(localStorage.getItem("myLeads"))
+console.log(leadsFromLocalstorage)
 
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
+    localStorage.setItem("myLeads", JSON.stringify(myLeads))
+
     renderLeads()
 })
 function renderLeads() {
@@ -22,3 +51,4 @@ function renderLeads() {
     }
     ulEl.innerHTML = listItems
 }
+
